94f4c8b59eba95f24003e256c97f71577c9beb13
clapynick <clapynick@gmail.com>
2018-06-07 22:45:40 +1000

    Created flatten file and readme

---
+++ flatten.py
@@ -0,0 +1,16 @@
+'''
+    File: Recursive flatten function
+    Author: N. Parsons
+    Copyright: N. Parsons (C) 2018
+'''
+def flatten(arr): # flattnes an n-dimensional list
+    if type(arr) is not list:
+       return [arr]
+    else:
+        if(len(arr) > 1):
+            return flatten(arr[0]) + flatten(arr[1:])
+        else:
+            return flatten(arr[0])
+
+a = [[1, 2, [3, 4]], [5, 6], 7, [8, [9, [10, [11, [12, [13, 14, [[[15], 16]]]]]]]]]
+print(flatten(a)) # output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 13, 14, 15, 16]
